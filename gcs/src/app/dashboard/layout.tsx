cfd9a944dffe475d9db7fd677401ba5314c1b1d8
SHIGEKUNI <zakiru@ezweb.ne.jp>
2024-09-16 11:39:37 +0900

    feat: header design

--- gcs/src/app/dashboard/layout.tsx
+++ gcs/src/app/dashboard/layout.tsx
@@ -6,10 +6,40 @@ export default function DashboardLayout({
   return (
     <section>
       {/* Header */}
-      <header className="bg-sky-500/100">
+      <header className="bg-neutral-900 h-13 ">
+        <nav className="px-6 h-14 w-full flex items-center">
+          <svg
+            className="size-8 text-blue-500"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
+            <line x1="9" y1="3" x2="9" y2="21" />
+          </svg>
+          <div className="ml-auto h-full flex items-center py-2">
+            <div className=" h-full flex items-center pr-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-11 text-blue-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+              </svg>
+            </div>
+          </div>
+        </nav>
       </header>
-      <nav></nav>
-
       {children}
     </section>
   );
